902d2c84e3ec7658af571f5031ee9e999c427f47
Szíriusz <milegyen0000@gmail.com>
2025-11-09 00:28:20 +0100

    hangok hosszabbodtak

--- audio.js
+++ audio.js
@@ -16,12 +16,23 @@ export function playSound(type) {
         complete: 660
     };
 
+    // Hang hosszúságok (másodpercben)
+    const durations = {
+        purchase: 0.3,
+        cut: 0.4,
+        sell: 0.25,
+        build: 0.35,
+        complete: 0.5
+    };
+
+    const duration = durations[type] || 0.3;
+
     oscillator.frequency.value = frequencies[type] || 440;
     oscillator.type = 'square'; // 8-bites hang
-    gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
-    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.1);
+    gainNode.gain.setValueAtTime(0.15, audioContext.currentTime);
+    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + duration);
 
     oscillator.start(audioContext.currentTime);
-    oscillator.stop(audioContext.currentTime + 0.1);
+    oscillator.stop(audioContext.currentTime + duration);
 }
 
